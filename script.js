342a052702813ed7326332eec41924f8fb0bf72e
Noam <noamru22@gmail.com>
2020-07-29 20:49:49 +0200

    everything works via classes now

--- script.js
+++ script.js
@@ -30,6 +30,10 @@ window.onload = function () {
   let aboutMaximize = document.querySelector(".about-maximize");
   let aboutTextbox = document.querySelector(".textbox-about");
   let contactTextbox = document.querySelector(".textbox-contact");
+  const gerritWindow = document.querySelector(".gerrit-window");
+  const spotavibeWindow = document.querySelector(".spotavibe-window");
+  const musicWindow = document.querySelector(".music-window");
+  const robotWindow = document.querySelector(".robot-window");
 
   /* SCREENSAVER */
 
@@ -89,22 +93,163 @@ window.onload = function () {
   /* PROJECTS */
   // MARIPOSA
   // store objects
-  let mariposaProject = document.getElementsByClassName("mariposa-project")[0];
-  let mariposaDesktop = document.querySelector(".mariposa");
-  let mariposaMenu = document.querySelector(".mariposa-menu");
+  const mariposaProject = document.getElementsByClassName(
+    "mariposa-project"
+  )[0];
+  const mariposaMenu = document.querySelector(".mariposa-menu");
   closemeMariposa = document.querySelector(".closeme-mariposa");
-  let mariposaMaximize = document.querySelector(".mariposa-maximize");
+  const mariposaMaximize = document.querySelector(".mariposa-maximize");
   let clickmeMariposa = document.getElementsByClassName("clickme-mariposa");
+  const clickGerritMenu = document.querySelector(".gerrit-menu");
+  const clickMusicMenu = document.querySelector(".music-menu");
+  const clickRobotMenu = document.querySelector(".robot-menu");
+  const clickSpotavibeMenu = document.querySelector(".spotavibe-menu");
+  const spotavibeDesktop = document.querySelector(".spotavibe");
+  const gerritDesktop = document.querySelector(".gerrit");
+  const musicDesktop = document.querySelector(".music-vis");
+  const robotDesktop = document.querySelector(".singing-robot");
+  const mariposaDesktop = document.querySelector(".mariposa");
 
-  // open project when doubleclick on desktop icon
-  mariposaDesktop.addEventListener("dblclick", function () {
-    openMariposa();
-  });
+  const props = {
+    menuButton: clickAboutMenu,
+    windowElement: aboutWindow,
+    iconClassName: "about-icon",
+    taskText: "about.txt - Notepad",
+  };
 
-  // open project when submenu clicked
-  mariposaMenu.addEventListener("click", function () {
-    openMariposa();
-  });
+  const contactProps = {
+    menuButton: clickContactMenu,
+    windowElement: contactWindow,
+    iconClassName: "contact-icon",
+    taskText: "contact me",
+  };
+
+  const gerritProps = {
+    desktopButton: gerritDesktop,
+    menuButton: clickGerritMenu,
+    windowElement: gerritWindow,
+    iconClassName: "gerrit-icon",
+    taskText: "gerrit.txt",
+  };
+
+  const musicProps = {
+    desktopButton: musicDesktop,
+    menuButton: clickMusicMenu,
+    windowElement: musicWindow,
+    iconClassName: "music-icon",
+    taskText: "music-visualization.txt",
+  };
+
+  const robotProps = {
+    desktopButton: robotDesktop,
+    menuButton: clickRobotMenu,
+    windowElement: robotWindow,
+    iconClassName: "robot-icon",
+    taskText: "tibetan-singing-robot.txt",
+  };
+
+  const spotavibeProps = {
+    desktopButton: spotavibeDesktop,
+    menuButton: clickSpotavibeMenu,
+    windowElement: spotavibeWindow,
+    iconClassName: "spotavibe-icon",
+    taskText: "spotavibe.txt",
+  };
+
+  const mariposaProps = {
+    desktopButton: mariposaDesktop,
+    menuButton: clickMariposaMenu,
+    windowElement: mariposaWindow,
+    iconClassName: "mariposa-icon",
+    taskText: "mariposa.txt",
+  }
+
+  class Window {
+    constructor(props) {
+      this.props = props;
+      this.isOpen = false;
+      this.getWindowButtons();
+      this.createEventHandler();
+    }
+
+    createTask() {
+      const task = document.createElement("div");
+      const icon = document.createElement("div");
+      const tasksList = document.getElementById("tasks");
+      const content = document.createTextNode(this.props.taskText);
+      icon.classList.add(this.props.iconClassName);
+      task.appendChild(icon);
+      task.appendChild(content);
+      task.classList.add("task");
+      tasksList.append(task);
+      this.task = task;
+      task.addEventListener("click", () => {
+        this.toggleWindow();
+      });
+    }
+
+    createEventHandler() {
+      this.props.menuButton.addEventListener("click", () => {
+        if (this.task) {
+          if (!this.isOpen) {
+            this.toggleWindow();
+          }
+          return;
+        }
+        this.createTask();
+        this.toggleWindow();
+        starField();
+      });
+      if (this.props.desktopButton) {
+        this.props.desktopButton.addEventListener("dblclick", () => {
+          if (this.task) {
+            if (!this.isOpen) {
+              this.toggleWindow();
+            }
+            return;
+          }
+          this.createTask();
+          this.toggleWindow();
+        });
+      }
+      this.maximize.addEventListener("click", () => {
+        this.textbox.classList.toggle("max");
+        this.props.windowElement.classList.toggle("max");
+        starField();
+      });
+      this.minimize.addEventListener("click", () => {
+        this.toggleWindow();
+      });
+      this.close.addEventListener("click", () => {
+        this.toggleWindow();
+        this.task.remove();
+        this.task = undefined;
+      });
+    }
+
+    toggleWindow() {
+      this.isOpen = !this.isOpen;
+      this.props.windowElement.hidden = !this.isOpen;
+      this.task.classList.toggle("active");
+    }
+
+    getWindowButtons() {
+      const { windowElement } = this.props;
+      this.maximize = windowElement.querySelector("[data-maximize]");
+      this.minimize = windowElement.querySelector("[data-minimize]");
+      this.close = windowElement.querySelector("[data-close]");
+      this.textbox = windowElement.querySelector("[data-textbox]");
+    }
+  }
+
+  // create window about and contact
+  new Window(props);
+  new Window(contactProps);
+  new Window(gerritProps);
+  new Window(spotavibeProps);
+  new Window(robotProps);
+  new Window(musicProps);
+  new Window(mariposaProps);
 
   /* DRAGGABLE */
   // Make the desktop icons draggable
@@ -116,6 +261,7 @@ window.onload = function () {
   // make windows draggable
   dragElement(aboutWindow);
   dragElement(contactWindow);
+  dragElement(gerritWindow);
 
   // make projects draggable
   dragElement(mariposaProject);
@@ -187,173 +333,4 @@ window.onload = function () {
       document.onmousemove = null;
     }
   }
-
-  function openMariposa() {
-    // change color desktop icon
-    mariposaDesktop.style.backgroundColor = "#00207d";
-    setInterval(function () {
-      mariposaDesktop.style.backgroundColor = "transparent";
-    }, 300);
-
-    // open project window
-    mariposaProject.hidden = false;
-    openMariposaProject = true;
-    // add Mariposa to taskbar
-    createMariposaTask();
-
-    // make sure all right-side buttons work
-    closeMariposa();
-    maxMariposa();
-    openMinimizeMariposa();
-
-    function createMariposaTask() {
-      if (!document.querySelector(".mariposa-task")) {
-        console.log("mariposa is not there and we are creating it now");
-        mariposaTask = document.createElement("div");
-        let mariposaIcon = document.createElement("div");
-        mariposaIcon.classList.add("mariposa-icon");
-        mariposaTask.appendChild(mariposaIcon);
-        let mariposaContent = document.createTextNode("mariposa.txt - Notepad");
-        mariposaTask.appendChild(mariposaContent);
-        mariposaTask.classList.add("mariposa-task");
-        mariposaTask.classList.add("clickme-mariposa");
-        let parentDiv = document.getElementById("placeholder-tasks").parentNode;
-        let newDiv = document.getElementById("placeholder-tasks");
-        console.log(newDiv);
-        parentDiv.insertBefore(mariposaTask, newDiv);
-        document
-          .getElementsByClassName("mariposa-task")[0]
-          .classList.add("active");
-      }
-      // make sure taskbar is still responsive
-      mariposaTask.onclick = function () {
-        mariposaProject.hidden = !openMariposaProject;
-        openMariposaProject = !openMariposaProject;
-        mariposaTask.classList.toggle("active");
-      };
-    }
-
-    function closeMariposa() {
-      // close window
-      closemeMariposa.onclick = function () {
-        mariposaTask.remove();
-        clickmeMariposa = document.getElementsByClassName("clickme-mariposa");
-        mariposaProject.hidden = true;
-        console.log("CLOSE Mariposa");
-        clickmeMariposa.onclick = function () {
-          createMariposaTask();
-          MariposaProject.hidden = false;
-          clickmeMariposa();
-        };
-      };
-    }
-
-    function maxMariposa() {
-      console.log(mariposaMaximize);
-      let maxMariposa = false;
-      mariposaMaximize.onclick = function () {
-        console.log("MAXXXXXX");
-        mariposaProject.classList.toggle("max");
-        // mariposaTextbox.classList.toggle("max");
-        maxMariposa = !maxMariposa;
-      };
-    }
-
-    function openMinimizeMariposa() {
-      // project is minized/opened when clicked on on the minimize button or task
-      for (let i = 0; i < clickmeMariposa.length; i++) {
-        clickmeMariposa[i].onclick = function () {
-          // open window
-          mariposaProject.hidden = !openMariposaProject;
-          openMariposaProject = !openMariposaProject;
-          // change styling task
-          if (mariposaTask) {
-            mariposaTask.classList.toggle("active");
-          }
-        };
-      }
-    }
-  }
-
-  const props = {
-    menuButton: clickAboutMenu,
-    windowElement: aboutWindow,
-    iconClassName: "about-icon",
-    taskText: "about.txt - Notepad",
-  };
-
-  const contactProps = {
-    menuButton: clickContactMenu,
-    windowElement: contactWindow,
-    iconClassName: "contact-icon",
-    taskText: "contact me",
-  };
-
-  class Window {
-    constructor(props) {
-      this.props = props;
-      this.isOpen = false;
-      this.getWindowButtons();
-      this.createEventHandler();
-    }
-
-    createTask() {
-      const task = document.createElement("div");
-      const icon = document.createElement("div");
-      const tasksList = document.getElementById("tasks");
-      const content = document.createTextNode(this.props.taskText);
-      icon.classList.add(this.props.iconClassName);
-      task.appendChild(icon);
-      task.appendChild(content);
-      task.classList.add("task");
-      tasksList.append(task);
-      this.task = task;
-      task.addEventListener("click", () => {
-        this.toggleWindow();
-      });
-    }
-
-    createEventHandler() {
-      this.props.menuButton.addEventListener("click", () => {
-        if (this.task) {
-          if (!this.isOpen) {
-            this.toggleWindow();
-          }
-          return;
-        }
-        this.createTask();
-        this.toggleWindow();
-        starField();
-      });
-      this.maximize.addEventListener("click", () => {
-        this.textbox.classList.toggle("max");
-        this.props.windowElement.classList.toggle("max");
-      });
-      this.minimize.addEventListener("click", () => {
-        this.toggleWindow();
-      });
-      this.close.addEventListener("click", () => {
-        this.toggleWindow();
-        this.task.remove();
-        this.task = undefined;
-      });
-    }
-
-    toggleWindow() {
-      this.isOpen = !this.isOpen;
-      this.props.windowElement.hidden = !this.isOpen;
-      this.task.classList.toggle("active");
-    }
-
-    getWindowButtons() {
-      const { windowElement } = this.props;
-      this.maximize = windowElement.querySelector("[data-maximize]");
-      this.minimize = windowElement.querySelector("[data-minimize]");
-      this.close = windowElement.querySelector("[data-close]");
-      this.textbox = windowElement.querySelector("[data-textbox]");
-    }
-  }
-
-  new Window(props);
-  new Window(contactProps);
 };
